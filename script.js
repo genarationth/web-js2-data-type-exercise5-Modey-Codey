146c8346c3173abb0329aa7e62fbb701d6cc74e2
Modey Codey <phawngam@hotmail.com>
2024-05-13 20:12:27 +0700

    Ex5

---
+++ script.js
@@ -0,0 +1,16 @@
+const authorM = {
+    authorId: 2,
+    name: "Na",
+    address: {
+        houseNo: 98,
+        street: 1,
+        county: "Thailand",
+        postcode: 8989,
+    },
+    url: "https://www.youtube.com/watch?v=4ofJpOEXrZs"
+};
+
+console.log(authorM.authorId);
+console.log(authorM.name);
+console.log(authorM.address);
+console.log(authorM.url);
